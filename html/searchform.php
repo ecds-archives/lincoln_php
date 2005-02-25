1f7e801e70b31c8c75c002ebbec6184e2f38e339
tbhasin <tbhasin@e3be4581-0c34-0410-b1bb-dee61535bed8>
2005-02-25 20:41:26 +0000

    Removed synonym search bec of no disctionary

--- html/searchform.php
+++ html/searchform.php
@@ -3,23 +3,14 @@ include("config.php");
 html_head("Search", "search.css");
 
 include("header.html");
+?>
 
-print "<div class='content' id='search'>";
 
-print '
-<ul class ="searchtips"><b>Search tips:</b>
-<li>Search terms are matched against <i>whole words</i></li>
-<li>Multiple words are allowed.</li>
-<li>Asterisks may be used when using a part of a word or words. <br>
-For example, enter <b>resign*</b> to match <b>resign</b>, <b>resigned</b>, and
-<b>resignation</b>. </li>
-<li> Use several categories to narrow your search. For example, use author, keyword and<br>
-title to match a particular sermon.</li>
-<li>When searching on a state, try the abbreviated form as well. For example, use NY and New York
-to see search results for both.</li>
-</ul>'; 
+<div class='content' id='search'>
 
-print '
+<table name="searchtable">
+<tr><td>
+
 <h2>Advanced Search</h2>
 
 <form name="sermonsquery" action="search.php" method="get">
@@ -31,29 +22,45 @@ print '
 <tr><th>Place of Publication</th><td class="input"><input type="text" size="40" name="place"></td></tr>
 <tr><td></td><td><input type="submit" value="Submit"> <input type="reset" value="Reset"></td></tr>
 </table>
-</form>';
+</form>
 
-print '
+
 <h2>Specialized Search</h2>
 <form name="advancedquery" action="search.php" method="get">
 <table class="searchform" border="0">
 <tr><th>Enter word or phrase:</th><td class="input"><input type="text" size="40" name="keyword"></td></tr>
 <tr><th>Type of search:</th><td><input type="radio" name="mode" value="phonetic">Phonetic
 <input type="radio" name="mode" value="exact">Exact Phrase
-<input type="radio" name="mode" value="synonym">Synonym</td>
+<!-- This requires a defined dictionary which we do not have yet<input type="radio" name="mode" value="synonym">Synonym</td>-->
 </tr>
 <tr><td></td><td><input type="submit" value="Submit"><input type="reset" value="Reset"></td></tr> 
 </table>
-</form>';
+</form>
+</td>
 
-print '
+<td class="searchtips">
+<ul class ="searchtips"><b>Search tips:</b>
+<li>Search terms are matched against <i>whole words</i></li>
+<li>Multiple words are allowed.</li>
+<li>Asterisks may be used when using a part of a word or words. <br>
+For example, enter <b>resign*</b> to match <b>resign</b>, <b>resigned</b>, and
+<b>resignation</b>. </li>
+<li> Use several categories to narrow your search. For example, use author, keyword and<br>
+title to match a particular sermon.</li>
+<li>When searching on a state, try the abbreviated form as well. For example, use NY and New York
+to see search results for both.</li>
+</ul>
+</td>
+</tr>
+</table>
+
 <p class="searchtips">If you are interested in doing a more complex search, please
 contact the <a href="mailto:beckcenter@emory.edu">Beck Center
-Staff</a>.</p> ';
+Staff</a>.</p>
 
 
-print "</div>";
-
+</div>
+<?php
 include("footer.html");
 ?>
 
