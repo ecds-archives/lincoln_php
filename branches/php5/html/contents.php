051c556be1f23fb92f8fe3eabc5288f436e23c85
nobody <nobody@e3be4581-0c34-0410-b1bb-dee61535bed8>
2005-02-25 23:05:32 +0000

    This commit was manufactured by cvs2svn to create branch 'php5'.

---
+++ branches/php5/html/contents.php
@@ -0,0 +1,53 @@
+<?php
+
+include("config.php");
+html_head("Contents", "lincoln.css");
+include_once("lib/taminoConnection.class.php");
+
+print "<body>";
+
+include("header.html");
+
+$args = array('host' => "vip.library.emory.edu",
+		'db' => "LINCOLN",
+	      	'coll' => 'sermons',
+	        'debug' => false );
+$tamino = new taminoConnection($args);
+
+/*
+$query = 'for $a in input()/TEI.2/:text/body/div1 
+let $bibl := $a/head/bibl  
+return <div> {$a/@id} {$bibl} </div> sort by (@id)';
+*/
+$query = 'for $a in input()/TEI.2
+let $auth := $a/teiHeader/fileDesc/titleStmt/author
+let $body := $a/:text/body
+return <div> {$auth}
+{for $div1 in $body/div1 return <div1>{$div1/@id}{$div1/head/bibl}</div1> } </div> sort by (author)'; 
+$xsl_file = "contents.xsl";  
+
+$rval = $tamino->xquery($query);
+if ($rval) {       // tamino Error code (0 = success)
+  print "<p>Error: failed to retrieve contents.<br>";
+  print "(Tamino error code $rval)</p>";
+  exit();
+} 
+
+
+print '<div class="content">  
+          <h2>Contents</h2>';
+print "<hr>";
+$xsl_file = "contents.xsl";
+$tamino->xslTransform($xsl_file); 
+$tamino->printResult(); 
+
+print "<hr>"; 
+print "</div>";
+
+include("footer.html");
+?> 
+   
+  </div>
+   
+</body>
+</html>
---
+++ branches/php5/html/sermon.php
@@ -0,0 +1,51 @@
+
+
+<?php
+include("config.php");
+html_head("Sermon", "sermons.css");
+
+include_once("lib/taminoConnection.class.php");
+
+print "<body>";
+
+include("header.html");
+
+$id = $_GET['id'];
+$terms = $_GET['term'];
+
+$args = array('host' => "vip.library.emory.edu",
+		'db' => "LINCOLN",
+	      	'coll' => 'sermons',
+	        'debug' => false);
+$tamino = new taminoConnection($args);
+
+$query = 'for $div in input()/TEI.2/:text/body/div1
+where $div/@id = "' . $id . '"
+return $div';
+$xsl_file = "sermon.xsl";
+
+$rval = $tamino->xquery($query);
+if ($rval) {       // tamino Error code (0 = success)
+  print "<p>Error: failed to retrieve contents.<br>";
+  print "(Tamino error code $rval)</p>";
+  exit();
+} 
+
+
+print '<div class="content">
+          <h2>Sermon</h2>';
+$tamino->highlightInfo($terms);
+
+$tamino->xslTransform($xsl_file);
+$tamino->printResult($terms);
+
+print "<p class='clear'>&nbsp;</p>";
+print "</div>";
+
+include("footer.html");
+?> 
+   
+  </div>
+   
+</body>
+</html>
