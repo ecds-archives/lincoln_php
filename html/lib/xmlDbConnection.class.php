b3777acee629f3b51f74a09cab3ef33cd3654c3a
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2005-10-26 23:37:07 +0000

    improvements from exist branch (print transformed xml, result links, etc.)

--- html/lib/xmlDbConnection.class.php
+++ html/lib/xmlDbConnection.class.php
@@ -1,6 +1,7 @@
 <?php 
 
 include "taminoConnection.class.php";
+//include "existConnection.class.php";
 
 class xmlDbConnection {
 
@@ -55,9 +56,7 @@ class xmlDbConnection {
     $this->xpath =& $this->xmldb->xpath;
 
     // variables for highlighting search terms
-    $this->begin_hi[0]  = "<span class='term1'>";
-    $this->begin_hi[1] = "<span class='term2'>";
-    $this->begin_hi[2] = "<span class='term3'>";
+    // begin highlighting variables are now defined when needed, according to number of terms
     $this->end_hi = "</span>";
   }
 
@@ -100,6 +99,24 @@ class xmlDbConnection {
      $this->xsl_result = $proc->transformToDoc($this->xmldb->xml);
    }
 
+   // transform the created xsl result  with a specified stylesheet
+   function xslTransformResult ($xsl_file, $xsl_params = NULL) {
+     /* load xsl & xml as DOM documents */
+     $xsl = new DomDocument();
+     $xsl->load("xsl/$xsl_file");
+
+     /* create processor & import stylesheet */
+     $proc = new XsltProcessor();
+     $xsl = $proc->importStylesheet($xsl);
+     if ($xsl_params) {
+       foreach ($xsl_params as $name => $val) {
+         $proc->setParameter(null, $name, $val);
+       }
+     }
+     /* transform the xsl result, and replace it with the result */
+     $this->xsl_result = $proc->transformToDoc($this->xsl_result);
+   }
+
    function printResult ($term = NULL) {
      if ($this->xsl_result) {
        if (isset($term[0])) {
@@ -115,6 +132,16 @@ class xmlDbConnection {
      }
 
    }
+
+   // create <span> tags for highlighting based on number of terms
+   function defineHighlight ($num) {
+     $this->begin_hi = array();
+    // strings for highlighting search terms 
+    for ($i = 0; $i < $num; $i++) {
+      $this->begin_hi[$i]  = "<span class='term" . ($i + 1) . "'>";
+    }
+   }
+
 
    // get the content of an xml node by name when the path is unknown
    function findNode ($name, $node = NULL) {
@@ -144,12 +171,17 @@ class xmlDbConnection {
      // Note: regexp is constructed to avoid matching/highlighting the terms in a url 
        $str = preg_replace("/([^=|']\b)($_term)(\b)/i",
 	      "$1" . $this->begin_hi[$i] . "$2$this->end_hi$3", $str);
+       // special case when term is at the beginning of string
+       $str = preg_replace("/(^)($_term)(\b)/i",
+        "$1" . $this->begin_hi[$i] . "$2$this->end_hi$3", $str);
      }
      return $str;
    }
 
    // highlight text in the xml structure
    function highlightXML ($term) {
+     // if span terms are not defined, define them now
+     if (!(isset($this->begin_hi))) { $this->defineHighlight(count($term)); }
      $this->highlight_node($this->xsl_result, $term);
    }
 
@@ -167,6 +199,8 @@ class xmlDbConnection {
    
    // print out search terms, with highlighting matching that in the text
    function highlightInfo ($term) {
+     // if span terms are not defined, define them 
+     if (!(isset($this->begin_hi))) { $this->defineHighlight(count($term)); }
      if (isset($term[0])) {
        print "<p align='center'>The following search terms have been highlighted: ";
        for ($i = 0; isset($term[$i]); $i++) {
@@ -175,6 +209,42 @@ class xmlDbConnection {
        print "</p>";
      }
    }
+
+   // print out links to different result sets, based on cursor
+   // arguments: url to link to (pos & max # to display will be added), max
+   function resultLinks ($url, $position, $max) {
+     //FIXME: at least in exist, we can get a default maximum from result set itself...
+     $result = "<div class='resultlink'>";
+	if ($this->count > $max) {
+	  $result .= "<li class='firstresultlink'>More results:</li";
+	  for ($i = 1; $i <= $this->count; $i += $max) {
+	    if ($i == 1) {
+	      $result .= '<li class="firstresultlink">';
+	    } else { 
+	      $result .= '<li class="resultlink">';
+	    }
+            // url should be based on current search url, with new position defined
+	    $myurl = $url .  "&pos=$i&max=$max";
+            if ($i != $position) {
+	      $result .= "<a href='$myurl'>";
+	    }
+    	    $j = min($this->count, ($i + $max - 1));
+    	    // special case-- last set only has one result
+    	    if ($i == $j) {
+      	      $result .= "$i";
+    	    } else {
+      	      $result .= "$i - $j";
+    	    }
+	    if ($i != $position) {
+      	      $result .= "</a>";
+    	    }
+    	    $result .= "</li>";
+	  }
+	}
+	$result .= "</div>"; 
+	return $result;
+   }
+
 
   // convert a readable xquery into a clean url for tamino or exist
   function encode_xquery ($string) {
@@ -189,12 +259,21 @@ class xmlDbConnection {
   }
 
    // print out xml (for debugging purposes)
-   function displayXML () {
-     if ($this->xml) {
-       $this->xml->formatOutput = true;
-       print "<pre>";
-       print htmlentities($this->xml->saveXML());
-       print "</pre>";
+   function displayXML ($transformed = 0) {
+     if ($transformed) { 	// display xml resulting from xsl transformation
+       if ($this->xsl_result) {
+         $this->xsl_result->formatOutput = true;
+         print "<pre>";
+         print htmlentities($this->xsl_result->saveXML());
+         print "</pre>";
+       }
+     } else {			// by default, display xml returned by query
+       if ($this->xml) {
+         $this->xml->formatOutput = true;
+         print "<pre>";
+         print htmlentities($this->xml->saveXML());
+         print "</pre>";
+       }
      }
    }
 
