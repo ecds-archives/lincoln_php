74395e1e9e838b17f75c5b8cb1136efc5e78c55d
rsutton <rsutton@e3be4581-0c34-0410-b1bb-dee61535bed8>
2005-11-07 22:41:18 +0000

    added lincoln icon for favicon (displayed by some browsers)

--- branches/php5/html/config.php
+++ branches/php5/html/config.php
@@ -28,6 +28,7 @@ function html_head ($pagetitle, $pagecss = "lincoln.css") {     // lincoln.css i
                 <title>The Martyred President : $pagetitle</title>
                 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">
                 <link rel=\"stylesheet\" type=\"text/css\" href=\"$pagecss\">
+		<link rel=\"shortcut icon\" href=\"images/lincoln.ico\">
              </head>";
 }
 ?>
