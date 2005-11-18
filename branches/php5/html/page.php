9df576032c0669a485d29867e859507620769e89
rsutton <rsutton@e3be4581-0c34-0410-b1bb-dee61535bed8>
2005-11-18 22:49:57 +0000

    switche to using common header, as in all other pages

--- branches/php5/html/page.php
+++ branches/php5/html/page.php
@@ -23,11 +23,12 @@ $tamino->xquery($query);
 
 $fig = $tamino->findNode("figDesc");
 
-print "<html>
+html_head("Page Image : $fig");
+/* print "<html>
          <head>
             <title>The Martyred President : Page Image : $fig</title>
              <link rel='stylesheet' type='text/css' href='lincoln.css'>
-";
+	     "; */
 $tamino->printResult();
 print "          </head>";
 
