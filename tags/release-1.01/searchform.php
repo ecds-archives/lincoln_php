3983ac04393c9496981472ad22cf8d16d1577c56
ahickco <ahickco@e3be4581-0c34-0410-b1bb-dee61535bed8>
2009-11-30 20:08:00 +0000

    adding google analytics and TEI badge

---
+++ tags/release-1.01/searchform.php
@@ -0,0 +1,12 @@
+<?php
+include("config.php");
+html_head("Search", "web/css/search.css");
+
+include("web/html/header.html");
+include("searchoptions.php");
+include("web/html/footer.html");
+include("web/html/google-tracklinc.xml");
+?>
+
+</body>
+</html>
---
+++ tags/release-1.01/thumbnail.php
@@ -0,0 +1,15 @@
+<?php
+include("config.php");
+html_head("Thumbnail View", "web/css/lincoln.css")
+
+include_once("config.php");
+include_once("xmlDbConnection.class.php");
+include("common_functions.php");
+
+print "<body>";
+
+include("web/html/header.html");
+
+
+include("web/html/footer.html");
+include("web/html/google-tracklinc.xml");
